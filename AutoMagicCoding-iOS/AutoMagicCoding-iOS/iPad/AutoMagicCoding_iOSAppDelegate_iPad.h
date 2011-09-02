069e478b562547edf6faa6bcd89b9bd04e73162c
Stepan Generalov <psi.pk.ru@gmail.com>
2011-09-02 23:10:04 +0400

    Added iOS Project. Transferred tests to it.

---
+++ AutoMagicCoding-iOS/AutoMagicCoding-iOS/iPad/AutoMagicCoding_iOSAppDelegate_iPad.h
@@ -0,0 +1,13 @@
+//
+//  AutoMagicCoding_iOSAppDelegate_iPad.h
+//  AutoMagicCoding-iOS
+//
+//  Created by Stepan Generalov on 02.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AutoMagicCoding_iOSAppDelegate.h"
+
+@interface AutoMagicCoding_iOSAppDelegate_iPad : AutoMagicCoding_iOSAppDelegate
+
+@end
---
+++ AutoMagicCoding-iOS/AutoMagicCoding-iOS/iPhone/AutoMagicCoding_iOSAppDelegate_iPhone.m
@@ -0,0 +1,13 @@
+//
+//  AutoMagicCoding_iOSAppDelegate_iPhone.m
+//  AutoMagicCoding-iOS
+//
+//  Created by Stepan Generalov on 02.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AutoMagicCoding_iOSAppDelegate_iPhone.h"
+
+@implementation AutoMagicCoding_iOSAppDelegate_iPhone
+
+@end
