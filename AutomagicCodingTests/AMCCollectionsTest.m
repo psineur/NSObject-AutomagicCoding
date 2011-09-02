4fa46dc5f37aa8d14917b28d6e587564c25e3e1d
Stepan Generalov <psi.pk.ru@gmail.com>
2011-09-02 11:46:25 +0400

    AMCCollectionsTest: added test with custom objects. It fails.
    TDD Rocks my World!

--- AutomagicCodingTests/AMCCollectionsTest.m
+++ AutomagicCodingTests/AMCCollectionsTest.m
@@ -9,6 +9,8 @@
 #import "AMCCollectionsTest.h"
 #import "FooWithCollections.h"
 #import "NSObject+AutomagicCoding.h"
+#import "Foo.h"
+#import "Bar.h"
 
 @implementation AMCCollectionsTest
 @synthesize fooWithCollections = _fooWithCollections;
@@ -150,16 +152,109 @@
     STAssertTrue([newFoo.dict count] == [self.fooWithCollections.dict count], @"newFoo.dict count is not wrong!" );
     
     STAssertTrue([newFoo.array isEqualTo: self.fooWithCollections.array], @"newFoo.array is not equal with the original!" );
-    STAssertTrue([newFoo.dict isEqualTo: self.fooWithCollections.dict], @"newFoo.dict is not equal with the original!" );}
+    STAssertTrue([newFoo.dict isEqualTo: self.fooWithCollections.dict], @"newFoo.dict is not equal with the original!" );
+}
 
 - (void) testCustomObjectsCollectionInMemory
 {
-    STFail(@"Test not implemented");
+    // Prepare custom object for collections.
+    Foo *one = [[Foo new] autorelease];
+    Foo *two = [[Foo new] autorelease];
+    Foo *three = [[Foo new] autorelease];
+    one.integerValue = 1;
+    two.integerValue = 2;
+    three.integerValue = 3;
+    
+    Bar *obj1 = [[Bar new] autorelease];
+    Bar *obj2 = [[Bar new] autorelease];
+    Bar *obj3 = [[Bar new] autorelease];
+    obj1.someString = @"object numero uno!";
+    obj2.someString = @"object numero dvuno!";
+    obj3.someString = @"object numero trino!";
+    
+    
+    // Prepare source foo.
+    self.fooWithCollections.array =[NSArray arrayWithObjects: one, two, three, nil];
+    self.fooWithCollections.dict = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects: obj1, obj2, obj3, nil] 
+                                                               forKeys:[NSArray arrayWithObjects: @"key1", @"key2", @"key3", nil]];
+    
+    // Save & Load.
+    NSDictionary *fooDict = [self.fooWithCollections dictionaryRepresentation];   
+    // Create new object from that dictionary.
+    FooWithCollections *newFoo = [[FooWithCollections objectWithDictionaryRepresentation: fooDict] retain];
+    
+    // Test newFoo.
+    STAssertNotNil(newFoo, @"newFoo failed to create.");
+    
+    if (![[newFoo className] isEqualToString: [FooWithCollections className]])
+        STFail(@"newFoo should be FooWithCollections!");
+    STAssertTrue( [newFoo isMemberOfClass: [FooWithCollections class]], @"newFoo is NOT MemberOfClass FooWithCollections!" );
+    
+    // Test newFoo collections.
+    STAssertNotNil(newFoo.array, @"newFoo.array failed to create.");
+    STAssertNotNil(newFoo.dict, @"newFoo.dict failed to create.");    
+    STAssertTrue( [newFoo.array isKindOfClass: [NSArray class]], @"newFoo.array is NOT KindOfClass NSArray!" );    
+    STAssertTrue( [newFoo.dict isKindOfClass: [NSDictionary class]], @"newFoo.dict is NOT KindOfClass NSDictionary!" );
+    
+    // Test newFoo collections to be the same size.
+    STAssertTrue([newFoo.array count] == [self.fooWithCollections.array count], @"newFoo.array count is not wrong!" );
+    STAssertTrue([newFoo.dict count] == [self.fooWithCollections.dict count], @"newFoo.dict count is not wrong!" );
+    
+    STAssertTrue([newFoo.array isEqualTo: self.fooWithCollections.array], @"newFoo.array is not equal with the original!" );
+    STAssertTrue([newFoo.dict isEqualTo: self.fooWithCollections.dict], @"newFoo.dict is not equal with the original!" );
 }
 
 - (void) testCustomObjectsCollectionInFile
 {
-    STFail(@"Test not implemented");
+    // Prepare custom object for collections.
+    Foo *one = [[Foo new] autorelease];
+    Foo *two = [[Foo new] autorelease];
+    Foo *three = [[Foo new] autorelease];
+    one.integerValue = 1;
+    two.integerValue = 2;
+    three.integerValue = 3;
+    
+    Bar *obj1 = [[Bar new] autorelease];
+    Bar *obj2 = [[Bar new] autorelease];
+    Bar *obj3 = [[Bar new] autorelease];
+    obj1.someString = @"object numero uno!";
+    obj2.someString = @"object numero dvuno!";
+    obj3.someString = @"object numero trino!";
+    
+    
+    // Prepare source foo.
+    self.fooWithCollections.array =[NSArray arrayWithObjects: one, two, three, nil];
+    self.fooWithCollections.dict = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects: obj1, obj2, obj3, nil] 
+                                                               forKeys:[NSArray arrayWithObjects: @"key1", @"key2", @"key3", nil]];
+    
+    // Save object representation in PLIST & Create new object from that PLIST.
+    NSString *path = [self testFilePathWithSuffix:@"Custom"];
+    NSDictionary *dictRepr =[self.fooWithCollections dictionaryRepresentation];    
+    [dictRepr writeToFile: path atomically:YES]; 
+    FooWithCollections *newFoo = [[FooWithCollections objectWithDictionaryRepresentation: [NSDictionary dictionaryWithContentsOfFile: path]] retain];
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath: path ])
+        STFail(@"Test file with path = %@ not exist! Dictionary representation = %@", path, dictRepr);
+    
+    // Test newFoo.
+    STAssertNotNil(newFoo, @"newFoo failed to create.");
+    
+    if (![[newFoo className] isEqualToString: [FooWithCollections className]])
+        STFail(@"newFoo should be FooWithCollections!");
+    STAssertTrue( [newFoo isMemberOfClass: [FooWithCollections class]], @"newFoo is NOT MemberOfClass FooWithCollections!" );
+    
+    // Test newFoo collections.
+    STAssertNotNil(newFoo.array, @"newFoo.array failed to create.");
+    STAssertNotNil(newFoo.dict, @"newFoo.dict failed to create.");    
+    STAssertTrue( [newFoo.array isKindOfClass: [NSArray class]], @"newFoo.array is NOT KindOfClass NSArray!" );    
+    STAssertTrue( [newFoo.dict isKindOfClass: [NSDictionary class]], @"newFoo.dict is NOT KindOfClass NSDictionary!" );
+    
+    // Test newFoo collections to be the same size.
+    STAssertTrue([newFoo.array count] == [self.fooWithCollections.array count], @"newFoo.array count is not wrong!" );
+    STAssertTrue([newFoo.dict count] == [self.fooWithCollections.dict count], @"newFoo.dict count is not wrong!" );
+    
+    STAssertTrue([newFoo.array isEqualTo: self.fooWithCollections.array], @"newFoo.array is not equal with the original!" );
+    STAssertTrue([newFoo.dict isEqualTo: self.fooWithCollections.dict], @"newFoo.dict is not equal with the original!" );
 }
 
 - (void) testCollectionsOfCollectionsInMemory
